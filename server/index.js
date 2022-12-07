62ba16f332c6d2033ebc693226f699ed897e756b
khampanpare88 <khampanpare88@gmail.com>
2022-12-07 12:41:35 +0700

    test

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import coursesRouters from './routes/courses.js';
 import skillRouters from './routes/skills.js';
 import updateCoursesSkill from './routes/courses.js';
 import newSkill from './routes/skills.js';
-
+import getCareer from './routes/career.js';
+import newCareer from './routes/career.js';
+import getStudent from './routes/student.js';
 
 const app = express();
 
@@ -17,7 +19,8 @@ app.use(cors());
 
 app.use('/courses' ,coursesRouters ,updateCoursesSkill);
 app.use('/skills',skillRouters ,newSkill);
-
+app.use('/career' , getCareer ,newCareer);
+app.use('/student' , getStudent);
 
 
 //const CONNECTION_URL = 'mongodb+srv://project:491@skillmapping.s8p064c.mongodb.net/?retryWrites=true&w=majority'
